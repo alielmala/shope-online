45d34744c9c1c91a7af6f5b702bc21f5f1fce4b1
alielmala <ali.elmala22@gmail.com>
2022-07-16 16:37:50 +0200

    initial navbar and header

---
+++ js/slider.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $('.header-slider').slick({
+        arrows: false,
+        centerPadding: "0px",
+        slidesToShow: 1,
+        useCSS: false,
+        autoplaySpeed:1500,
+        speed:0,
+        infinite: true,
+        autoplay: true,
+    });
+    $
+  });
