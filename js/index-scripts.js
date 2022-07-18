0d0c1f346f91f60420609c73f49d76b4bdc34fc4
alielmala <ali.elmala22@gmail.com>
2022-07-18 23:27:52 +0200

    latest-offers

---
+++ js/index-scripts.js
@@ -0,0 +1,4 @@
+var itemSaledPrice = document.getElementsByClassName("item-price-saled");
+var sale = document.getElementsByClassName("item-sale");
+document.getElementsByClassName("item-price-new").innerHTML="50";
+itemSaledNewPrice.innerHTML="sale/100*itemSaledPrice";
